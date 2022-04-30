c0e0d51f8a31fc881f648162f1f9c3109f7c31e0
Fernando Falci <fernando.falci@adevinta.com>
2022-04-30 21:24:46 +0200

    Add audio background

---
+++ script.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Authenticate
+## Twitter
+echo "$TWURLRC" > ~/.twurlrc
+
+## Youtube
+YT_TOKEN=`curl https://www.googleapis.com/oauth2/v4/token \
+    -d client_id=$YT_CLIENT_ID \
+    -d client_secret=$YT_CLIENT_SECRET \
+    -d refresh_token=$YT_REFRESH_TOKEN \
+    -d grant_type=refresh_token -s | jq -r '.access_token'`
+
+# Load content
+DESC=`cat ./description.txt`
+SHARE=`cat ./share.txt`
+
+# Tweet
+TWEET_ID=`twurl -d "status=$SHARE" /1.1/statuses/update.json | jq -r '.id_str'`
+echo "Tweeted: https://twitter.com/FernandoFalci/status/$TWEET_ID"
+
+TITLE=$(echo "$SHARE" | head -n 1)
+DAY=$(echo $TITLE | awk '{print $2}')
+SCORE=$(echo $TITLE | awk '{print $3}')
+DESC="$SHARE $DESC"
+
+# Youtube
+## 1. Create a video resource
+LOCATION=`curl "https://www.googleapis.com/upload/youtube/v3/videos?uploadType=resumable&part=snippet,status&key=$YT_CLIENT_ID" \
+    --header "Authorization: Bearer $YT_TOKEN" \
+    --header 'Accept: application/json' \
+    --header 'Content-Type: application/json' \
+    --data '{"snippet": {"title": "'"$TITLE"'","description": "'"$DESC"'","tags": ["wordle", "day'"$DAY"'", "shorts"]},"status": {"privacyStatus": "public"}}' \
+    --dump-header /dev/fd/1 \
+    --silent | grep location | awk '{print $2}'`
+
+## 2. Check if there's quota
+if [ -z "$LOCATION" ]; then
+    echo "Error: Could not upload the video to YouTube."
+    exit 0;
+fi
+
+echo "Created video: $LOCATION"
+
+## 3. Add a background audio
+### Choose a music
+AUDIOS=(./audio/*.mp3)
+AUDIO_INDEX=$(($DAY % ${#AUDIOS[@]}))
+AUDIO=${AUDIOS[$AUDIO_INDEX]}
+
+### Video + music
+ffmpeg -loglevel error -i video.mp4 -i "$AUDIO" -map 0:v -map 1:a -c:v copy -shortest output.mp4 
+
+## 4. Upload the video
+VIDEO_ID=$(curl $LOCATION \
+    --header "Authorization: Bearer $YT_TOKEN" \
+    --header 'Content-Type: application/octet-stream' \
+    --data-binary @output.mp4 -s | jq -r '.id')
+
+echo "Uploaded video: https://youtu.be/$VIDEO_ID"
+
+## 5. Tweet a reply to the first tweet.
+TWEET_TEXT2="https://youtu.be/$VIDEO_ID"
+twurl -d "in_reply_to_status_id=$TWEET_ID&status=$TWEET_TEXT2" /1.1/statuses/update.json
